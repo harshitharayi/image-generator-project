0f00113632b25e36548fdc3e647d6ab9f55c8f56
harshitharayi <139128140+harshitharayi@users.noreply.github.com>
2024-06-30 11:34:11 -0700

    Updated script.js by commenting API Key

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "sk-proj-3y0GfwjVuZQlCpyP891xT3BlbkFJgbNhNgIRTrdCf2t5h57L"; 
+//const OPENAI_API_KEY = "sk-proj-3y0GfwjVuZQlCpyP891xT3BlbkFJgbNhNgIRTrdCf2t5h57L"; 
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
